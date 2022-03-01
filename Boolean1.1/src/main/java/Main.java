8a7adb847c7cbc379cdc56094c0f3cd214ced48f
AetemRadchuk <artem163ru199@mail.ru>
2022-03-01 17:55:15 +0400

    изменил операторы

--- Boolean1.1/src/main/java/Main.java
+++ Boolean1.1/src/main/java/Main.java
@@ -1,19 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
-        int milkAmount = 300; // ml
+        int milkAmount = 200; // ml
         int powderAmount = 5; // g
-        int eggsCount = 5; // items
+        int eggsCount = 3; // items
         int sugarAmount = 5; // g
         int oilAmount = 30; // ml
         int appleCount = 8;
 
-        // Example
-        // appleCount - 5
+
         if (powderAmount >= 400 & sugarAmount >= 10 & milkAmount >= 1000 & oilAmount >= 30 ) {
             System.out.println("Pancakes");
         }
-        if (milkAmount >= 150 & powderAmount >= 3 & eggsCount >= 3){
+        if (milkAmount <= 300 & powderAmount >= 5 & eggsCount <= 5){
             System.out.println("Omelette");
         }
         if (appleCount >= 3 & milkAmount >= 100 & powderAmount >= 300 & eggsCount >= 4){
