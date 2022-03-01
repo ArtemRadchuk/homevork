ed08c47cd19551885012f0947b9a4ffcf9a210dd
AetemRadchuk <artem163ru199@mail.ru>
2022-03-01 16:51:40 +0400

    1.0

--- Boolean1.1/src/main/java/Main.java
+++ Boolean1.1/src/main/java/Main.java
@@ -19,13 +19,5 @@ public class Main {
         if (appleCount >= 3 & milkAmount >= 100 & powderAmount >= 300 & eggsCount >= 4){
             System.out.println("Apple pie");
         }
-        //powder - 400 g, sugar - 10 g, milk - 1 l, oil - 30 ml
-        System.out.println("Pancakes");
-
-        //milk - 300 ml, powder - 5 g, eggs - 5
-        System.out.println("Omelette");
-
-        //apples - 3, milk - 100 ml, powder - 300 g, eggs - 4
-        System.out.println("Apple pie");
     }
 }
