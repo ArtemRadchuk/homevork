6182f910a17ebfaff81652ae456d8d082d8f9944
AetemRadchuk <artem163ru199@mail.ru>
2022-03-03 11:24:31 +0400

    Добавление параметра вес и итогового веса корзины

--- ObjectAndClasses/Basket/totalWeight/src/main/java/Basket.java
+++ ObjectAndClasses/Basket/totalWeight/src/main/java/Basket.java
@@ -1,79 +1,85 @@
 public class Basket {
-
-    private static int count = 0;
-    private String items = "";
-    private int totalPrice = 0;  //Итоговая цена
-    private int limit; //Лимит
-    private int totalWeight = 0; //Итоговый вес
-    public Basket() {
+private static int count = 0;
+private String items = "";
+private int totalPrice = 0;
+private int limit;
+private double totalWeight = 0; //Итоговый вес
+private double weight = 0;//Инициализация переменной weight
+public Basket() {
         increaseCount(1);
         items = "Список товаров:";
         this.limit = 1000000;
-    }
+        }
 
-    public Basket(int limit) {
+public Basket(int limit) {
         this();
         this.limit = limit;
-    }
+        }
 
-    public Basket(String items, int totalPrice) {
+public Basket(String items, int totalPrice) {
         this();
         this.items = this.items + items;
         this.totalPrice = totalPrice;
-    }
+        }
 
-    public static int getCount() {
+public static int getCount() {
         return count;
-    }
+        }
 
-    public static void increaseCount(int count) {
+public static void increaseCount(int count) {
         Basket.count = Basket.count + count;
-    }
+        }
 
-    public void add(String name, int price) {
+public void add(String name, int price) {
         add(name, price, 1);
-    }
+        }
+public void add(String name, int price, int count, double weight) {   //add с добавленным параметром weight
+        this.weight = weight;
+        }
 
-    public void add(String name, int price, int count) {
+public void add(String name, int price, int count) {
         boolean error = false;
         if (contains(name)) {
-            error = true;
+        error = true;
         }
 
         if (totalPrice + count * price >= limit) {
-            error = true;
+        error = true;
         }
 
         if (error) {
-            System.out.println("Error occured :(");
-            return;
+        System.out.println("Error occured :(");
+        return;
         }
 
         items = items + "\n" + name + " - " +
-                count + " шт. - " + price;
+        count + " шт. - " + price;
         totalPrice = totalPrice + count * price;
-    }
+        totalWeight = totalWeight + count * weight; //Рассчет веса корзины
+        }
 
-    public void clear() {
+public void clear() {
         items = "";
         totalPrice = 0;
+        }
+public double getTotalWeight() {  //Итоговый вес корзины
+    return totalWeight;
     }
 
-    public int getTotalPrice() {
+public int getTotalPrice() {
         return totalPrice;
-    }
+        }
 
-    public boolean contains(String name) {
+public boolean contains(String name) {
         return items.contains(name);
-    }
+        }
 
-    public void print(String title) {
+public void print(String title) {
         System.out.println(title);
         if (items.isEmpty()) {
-            System.out.println("Корзина пуста");
+        System.out.println("Корзина пуста");
         } else {
-            System.out.println(items);
+        System.out.println(items);
         }
-    }
-}
-
+        }
+        }
