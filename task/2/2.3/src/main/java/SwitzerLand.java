6e0528b379c2d3b89b0342a5e8e05427e61aebda
AetemRadchuk <artem163ru199@mail.ru>
2022-03-05 16:40:59 +0400

    POJO класс SwitzerLand

---
+++ task/2/2.3/src/main/java/SwitzerLand.java
@@ -0,0 +1,26 @@
+public class SwitzerLand {
+    class Switzerland {
+        int square;
+        int populationSize;
+
+        public Switzerland(int square, int populationSize) {
+        }
+
+        public int getSquare() {
+            return square;
+        }
+
+        public void setSquare(int square) {
+            this.square = square;
+        }
+
+        public int getPopulationSize() {
+            return populationSize;
+        }
+
+        public void setPopulationSize(int populationSize) {
+            this.populationSize = populationSize;
+        }
+
+    }
+}
