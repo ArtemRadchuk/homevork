db1060c2de08c59cbe6350e9228b234c8061b385
AetemRadchuk <artem163ru199@mail.ru>
2022-03-09 13:50:11 +0400

    Клоны

--- ObjectAndClasses/Immutable/src/main/java/Main.java
+++ ObjectAndClasses/Immutable/src/main/java/Main.java
@@ -7,42 +7,45 @@ public class Main {
         private String regNum;
         private boolean brittle;
 
-        public cargo() {
+        public cargo(int mass, int size, String addres, boolean flip, String regNum, boolean brittle) {
         }
-    }
 
-    public final class Dimension {
-        private int width;
-        private int height;
-        private int lenght;
 
-        public void findVolume() {
-            int size = width * height * lenght;
+        public final class Dimension implements Cloneable {
+            private int width;
+            private int height;
+            private int lenght;
+
+            public void findVolume() {
+                int size = width * height * lenght;
+            }
+
+
+            public void currentDimension() {
+                int width = 0;
+                int height = 0;
+                int lenght = 0;
+                this.width = width;
+                this.height = height;
+                this.lenght = lenght;
+                return width.clone();
+                return height.clone();
+                return lenght.clone();
+            }
         }
-    }
 
-    public void currentDimension() {
-        int width = 0;
-        int height = 0;
-        int lenght = 0;
-        this.width = width;
-        this.height = height;
-        this.lenght = lenght;
-        return wigth.clone();
-        return height.clone();
-        return lenght.clone();
-    }
-    public void currentAddres () {
-        String addres = "";
-        this.addres = addres;
-        return addres.clone();
+        public void currentAddres() {
+            String addres = "";
+            this.addres = addres;
+            return addres.clone();
 
-    }
-        public void currentMass () {
+        }
+
+        public void currentMass() {
             int mass = 0;
             this.mass = mass;
             return mass.clone();
 
         }
-
     }
+}
