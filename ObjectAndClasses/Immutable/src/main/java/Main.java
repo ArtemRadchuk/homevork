cb3ea05c8ce9b1bc084beee81769928bcf80e71c
AetemRadchuk <artem163ru199@mail.ru>
2022-03-09 15:39:45 +0400

    Создан клон cargo

--- ObjectAndClasses/Immutable/src/main/java/Main.java
+++ ObjectAndClasses/Immutable/src/main/java/Main.java
@@ -1,51 +1,64 @@
 public class Main {
+
     public final class cargo {
         private int mass;
         private int size;
-        private String addres;
+        private String address;
         private boolean flip;
         private String regNum;
         private boolean brittle;
 
-        public cargo(int mass, int size, String addres, boolean flip, String regNum, boolean brittle) {
-        }
 
-
-        public final class Dimension implements Cloneable {
-            private int width;
-            private int height;
-            private int lenght;
+        public class Dimension implements Cloneable {
+            public int width;
+            public int height;
+            public int length;
 
             public void findVolume() {
-                int size = width * height * lenght;
+                int size = width * height * length;
             }
 
+            @Override
+            public cargo clone() throws CloneNotSupportedException {
+                int mass;
+                int size;
+                String address;
+                boolean flip;
+                String regNum;
+                boolean brittle;
+                return (cargo) super.clone();
+            }
 
-            public void currentDimension() {
+            public int getWidth(int width) {
+                return width;
+            }
+
+            public int getLength(int length) {
+                return length;
+            }
+
+            public int getHeight(int height) {
+                return height;
+            }
+
+            public void setCurrentDimension() {
                 int width = 0;
                 int height = 0;
-                int lenght = 0;
+                int length = 0;
                 this.width = width;
                 this.height = height;
-                this.lenght = lenght;
-                return width.clone();
-                return height.clone();
-                return lenght.clone();
+                this.length = length;
             }
         }
 
-        public void currentAddres() {
-            String addres = "";
-            this.addres = addres;
-            return addres.clone();
-
+        public void currentAddress() {
+            String address = "";
+            this.address = address;
         }
 
         public void currentMass() {
             int mass = 0;
             this.mass = mass;
-            return mass.clone();
-
         }
     }
 }
