6ae9ab316c32bdef34ba96214824b951b83e9715
AetemRadchuk <artem163ru199@mail.ru>
2022-03-09 13:20:40 +0400

    Груз

---
+++ ObjectAndClasses/Immutable/src/main/java/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+    public final class cargo {
+        private int mass;
+        private int size;
+        private String addres;
+        private boolean flip;
+        private String regNum;
+        private boolean brittle;
+
+        public cargo() {
+        }
+    }
+
+    public final class Dimension {
+        private int width;
+        private int height;
+        private int lenght;
+
+        public void findVolume() {
+            int size = width * height * lenght;
+        }
+    }
+
+    public void currentDimension() {
+        int width = 0;
+        int height = 0;
+        int lenght = 0;
+        this.width = width;
+        this.height = height;
+        this.lenght = lenght;
+        return wigth.clone();
+        return height.clone();
+        return lenght.clone();
+    }
+    public void currentAddres () {
+        String addres = "";
+        this.addres = addres;
+        return addres.clone();
+
+    }
+        public void currentMass () {
+            int mass = 0;
+            this.mass = mass;
+            return mass.clone();
+
+        }
+
+    }
