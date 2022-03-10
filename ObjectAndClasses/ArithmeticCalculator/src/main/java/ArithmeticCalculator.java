3569d47f237a57c09909f77c7f03f4862d793b38
AetemRadchuk <artem163ru199@mail.ru>
2022-03-10 11:02:53 +0400

    Инициализация и создание конструктора

---
+++ ObjectAndClasses/ArithmeticCalculator/src/main/java/ArithmeticCalculator.java
@@ -0,0 +1,9 @@
+public class ArithmeticCalculator {
+    int firstNum;
+    int secondNum;
+    public ArithmeticCalculator(int firstNum, int secondNum) {
+        this.firstNum = firstNum;
+        this.secondNum = secondNum;
+    }
+    public
+}
