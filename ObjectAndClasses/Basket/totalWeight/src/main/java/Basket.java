ff39f3c8b873e4d8e4c0646d77360c7b8307d9a0
AetemRadchuk <artem163ru199@mail.ru>
2022-03-10 10:16:55 +0400

    Добавление статических переменных количества товаров и итоговой стоимости товаров в корзине. Добавление метода расчета средней цены товаров в корзине.

--- ObjectAndClasses/Basket/totalWeight/src/main/java/Basket.java
+++ ObjectAndClasses/Basket/totalWeight/src/main/java/Basket.java
@@ -1,85 +1,103 @@
 public class Basket {
-private static int count = 0;
-private String items = "";
-private int totalPrice = 0;
-private int limit;
-private double totalWeight = 0; //Итоговый вес
-private double weight = 0;//Инициализация переменной weight
-public Basket() {
+    private static int count = 0;
+    private String items = "";
+    private int totalPrice = 0;
+    private int limit;
+    private double totalWeight = 0; //Итоговый вес
+    private double weight = 0;//Инициализация переменной weight
+    static int totalCost;
+    static int quantity;
+
+    public Basket() {
         increaseCount(1);
         items = "Список товаров:";
         this.limit = 1000000;
-        }
+    }
 
-public Basket(int limit) {
+    public Basket(int limit) {
         this();
         this.limit = limit;
-        }
+    }
 
-public Basket(String items, int totalPrice) {
+    public Basket(String items, int totalPrice) {
         this();
         this.items = this.items + items;
         this.totalPrice = totalPrice;
-        }
+    }
 
-public static int getCount() {
+    public static int getCount() {
         return count;
-        }
+    }
 
-public static void increaseCount(int count) {
+    public static void increaseCount(int count) {
         Basket.count = Basket.count + count;
-        }
+    }
 
-public void add(String name, int price) {
+    public static void addProduct(int quantity, int totalCost) {
+        this.quantity = this.quantity + quantity;
+        this.totalCost = this.totalCost + totalCost;
+        return quantity;
+        return totalCost;
+    }
+
+    public static void averagePrice(int quantity, int totalCost) {
+        quantity = this.quantity;
+        totalCost = this.totalCost;
+        System.out.println("Средняя цена товаров корзины - " + totalCost / quantity);
+    }
+
+    public void add(String name, int price) {
         add(name, price, 1);
-        }
-public void add(String name, int price, int count, double weight) {   //add с добавленным параметром  weight
-        this.weight = weight;
-        }
+    }
 
-public void add(String name, int price, int count) {
+    public void add(String name, int price, int count, double weight) {   //add с добавленным параметром  weight
+        this.weight = weight;
+    }
+
+    public void add(String name, int price, int count) {
         boolean error = false;
         if (contains(name)) {
-        error = true;
+            error = true;
         }
 
         if (totalPrice + count * price >= limit) {
-        error = true;
+            error = true;
         }
 
         if (error) {
-        System.out.println("Error occured :(");
-        return;
+            System.out.println("Error occured :(");
+            return;
         }
 
         items = items + "\n" + name + " - " +
-        count + " шт. - " + price;
+                count + " шт. - " + price;
         totalPrice = totalPrice + count * price;
         totalWeight = totalWeight + count * weight; //Рассчет веса корзины
-        }
-
-public void clear() {
-        items = "";
-        totalPrice = 0;
-        }
-public double getTotalWeight() {  //Итоговый вес корзины
-    return totalWeight;
     }
 
-public int getTotalPrice() {
+    public void clear() {
+        items = "";
+        totalPrice = 0;
+    }
+
+    public double getTotalWeight() {  //Итоговый вес корзины
+        return totalWeight;
+    }
+
+    public int getTotalPrice() {
         return totalPrice;
-        }
+    }
 
-public boolean contains(String name) {
+    public boolean contains(String name) {
         return items.contains(name);
-        }
+    }
 
-public void print(String title) {
+    public void print(String title) {
         System.out.println(title);
         if (items.isEmpty()) {
-        System.out.println("Корзина пуста");
+            System.out.println("Корзина пуста");
         } else {
-        System.out.println(items);
+            System.out.println(items);
         }
-        }
-        }
+    }
+}
