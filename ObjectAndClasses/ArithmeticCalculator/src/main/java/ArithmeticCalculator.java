e6c2faabe297f9f40561ab249fc7cf3979c6733f
AetemRadchuk <artem163ru199@mail.ru>
2022-03-10 15:12:28 +0400

    ArithmeticCalculator с enum

--- ObjectAndClasses/ArithmeticCalculator/src/main/java/ArithmeticCalculator.java
+++ ObjectAndClasses/ArithmeticCalculator/src/main/java/ArithmeticCalculator.java
@@ -1,9 +1,41 @@
 public class ArithmeticCalculator {
-    int firstNum;
-    int secondNum;
-    public ArithmeticCalculator(int firstNum, int secondNum) {
+    public static int firstNum;
+    public static int secondNum;
+
+    public ArithmeticCalculator() {
         this.firstNum = firstNum;
         this.secondNum = secondNum;
     }
-    public
+
+    public int getFirstNum() {
+        return firstNum;
+    }
+
+    public void setFirstNum(int firstNum) {
+        this.firstNum = firstNum;
+    }
+
+    public int getSecondNum() {
+        return secondNum;
+    }
+
+    public void setSecondNum(int secondNum) {
+        this.secondNum = secondNum;
+    }
+
+    public static void main(String[] args) {
+        Operation operation = Operation.ADD;
+        switch (operation) {
+            case ADD:
+            System.out.println(firstNum + secondNum);
+            break;
+            case SUBTRACT:
+                System.out.println(firstNum - secondNum);
+                break;
+            case MULTIPLY:
+                System.out.println((firstNum * secondNum));
+                break;
+        }
+    }
+
 }
