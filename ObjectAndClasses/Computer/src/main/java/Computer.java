ad0b5766ccce57aa3388d36f4363e38e8d623a77
AetemRadchuk <artem163ru199@mail.ru>
2022-03-11 12:19:32 +0400

    Добавлены геттеры и сетторы

--- ObjectAndClasses/Computer/src/main/java/Computer.java
+++ ObjectAndClasses/Computer/src/main/java/Computer.java
@@ -22,10 +22,52 @@ public class Computer {
         int currentMass = cpu.getCpuWeight() + keyboard.getKWeight() + monitor.getMWeight() + ram.getRamWeight() + drive.getDWeight();
         this.currentMass = currentMass;
     }
-    public int setCurrentMass(){
+
+    public int setCurrentMass() {
         return currentMass;
     }
-    public String toString(){
-        return "Производитель - " + vendor + ", модель - "+ name + ", диск " + drive + ", монитор - " + monitor + ", клавиатура" +  keyboard + ", процессор -" + cpu + ", оперативная память -" + ram + ", общей массой - " + currentMass;
+
+    public Drive getDrive() {
+        return drive;
+    }
+
+    public void setDrive(Drive drive) {
+        this.drive = drive;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public void setMonitor(Monitor monitor) {
+        this.monitor = monitor;
+    }
+
+    public Keyboard getKeyboard() {
+        return keyboard;
+    }
+
+    public void setKeyboard(Keyboard keyboard) {
+        this.keyboard = keyboard;
+    }
+
+    public CPU getCpu() {
+        return cpu;
+    }
+
+    public void setCpu(CPU cpu) {
+        this.cpu = cpu;
+    }
+
+    public RAM getRam() {
+        return ram;
+    }
+
+    public void setRam(RAM ram) {
+        this.ram = ram;
+    }
+
+    public String toString() {
+        return "Производитель - " + vendor + ", модель - " + name + ", диск " + drive + ", монитор - " + monitor + ", клавиатура" + keyboard + ", процессор -" + cpu + ", оперативная память -" + ram + ", общей массой - " + currentMass;
     }
 }
