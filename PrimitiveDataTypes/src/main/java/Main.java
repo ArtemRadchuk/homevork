3bbdd420559f17dedd86b472ccb4da8432225a30
AetemRadchuk <artem163ru199@mail.ru>
2022-03-16 00:19:03 +0400

    Выводит алфавит и код символа включая буквы Ё и ё

--- PrimitiveDataTypes/src/main/java/Main.java
+++ PrimitiveDataTypes/src/main/java/Main.java
@@ -4,13 +4,23 @@ public class Main {
         container.addCount(5672);
         System.out.println(container.getCount());
 
-
         // TODO: ниже напишите код для выполнения задания:
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
-        for (char a = 'А'; a <= 'я'; a ++) {
+        for (char a = 'А'; a <= 'я'; a++) {
             int b = a;
+            if (a == 'Ж'){
+                char e1 = 'Ё';
+                int ee1 = e1;
+                System.out.println(e1 + " - " + ee1);
+            }
+            if (a == 'ж'){
+                char e2 = 'ё';
+                int ee2 = e2;
+                System.out.println(e2 + " - " + ee2);
+            }
             System.out.println(a + " - " + b);
         }
+
     }
 }
