2ae4b7efa56a5e339ff6c2fd552ddfc228129f12
AetemRadchuk <artem163ru199@mail.ru>
2022-03-16 13:18:09 +0400

    Добавление нового класса

--- PrimitiveDataTypes/src/main/java/Main.java
+++ PrimitiveDataTypes/src/main/java/Main.java
@@ -7,20 +7,8 @@ public class Main {
         // TODO: ниже напишите код для выполнения задания:
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
-        for (char a = 'А'; a <= 'я'; a++) {
-            int b = a;
-            if (a == 'Ж'){
-                char e1 = 'Ё';
-                int ee1 = e1;
-                System.out.println(e1 + " - " + ee1);
-            }
-            if (a == 'ж'){
-                char e2 = 'ё';
-                int ee2 = e2;
-                System.out.println(e2 + " - " + ee2);
-            }
-            System.out.println(a + " - " + b);
-        }
+        Unicode test = new Unicode();
+        test.findUnicode();
 
     }
 }
