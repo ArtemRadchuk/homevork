8b0801d63c70a483c2557e9a29c56d74d4577219
AetemRadchuk <artem163ru199@mail.ru>
2022-03-17 17:32:47 +0400

    Поправил код

--- NumbersAndDates/src/main/java/practice/Birthdays.java
+++ NumbersAndDates/src/main/java/practice/Birthdays.java
@@ -1,20 +1,17 @@
 package practice;
 
-import javax.imageio.stream.ImageOutputStream;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
-import java.util.zip.DataFormatException;
 
 
 public class Birthdays {
 
     public static void main(String[] args) {
 
-        int day = 17;
+        int day = 31;
         int month = 3;
-        int year = 2021;
+        int year = 2001;
         System.out.println(collectBirthdays(year, month, day));
     }
 
@@ -24,12 +21,11 @@ public class Birthdays {
         int myYear = -1;
         StringBuilder result = new StringBuilder();
         for (; birthday.isBefore(today) || birthday.isEqual(today) == true; ) {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern( "dd.MM.yyyy - E", Locale.ENGLISH);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy - E", Locale.ENGLISH);
             String formattedString = birthday.format(formatter);
             myYear++;
             birthday = birthday.plusYears(1);
             result.append(myYear + " - " + formattedString + "\n");
-           // System.out.println(myYear + " - " + formattedString);
         }
         return result.toString();
     }
