f9764c4e666ebbd4f5a3f77fb1649eaa1d959263
AetemRadchuk <artem163ru199@mail.ru>
2022-03-17 10:12:18 +0400

    Грузовики и контейнеры

--- GeometryCalculator/src/main/java/practice/TrucksAndContainers.java
+++ GeometryCalculator/src/main/java/practice/TrucksAndContainers.java
@@ -9,6 +9,29 @@ public class TrucksAndContainers {
 
         //получение количество коробок от пользователя
         int boxes = scanner.nextInt();
+        int container = 0;
+        int truck = 0;
+        int loadedBox = 1;
+        if (boxes != 0) {
+            truck++;
+            container++;
+            System.out.println("Грузовик: " + truck);
+            System.out.println("\tКонтейнер: " + container);
+            for (; loadedBox <= boxes; loadedBox++) {
+                System.out.println("\t\tЯщик: " + loadedBox);
+                if (loadedBox % 324 == 0) {
+                    truck++;
+                    System.out.println("Грузовик: " + truck);
+                }
+                if (loadedBox % 27 == 0 && boxes > loadedBox) {
+                    container++;
+                    System.out.println("\tКонтейнер: " + container);
+                }
+            }
+        }
+        System.out.println("Необходимо:\nгрузовиков - " + truck + " шт.");
+        System.out.println("контейнеров - " + container + " шт.");
+
 
         // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
         // пример вывода при вводе 2
