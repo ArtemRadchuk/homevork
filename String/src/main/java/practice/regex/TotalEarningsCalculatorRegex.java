74bd91473dd397829b87136f93cdc8a0721f87a0
AetemRadchuk <artem163ru199@mail.ru>
2022-03-21 10:39:50 +0400

    Сумма заработка используя регулярки

--- String/src/main/java/practice/regex/TotalEarningsCalculatorRegex.java
+++ String/src/main/java/practice/regex/TotalEarningsCalculatorRegex.java
@@ -1,14 +1,28 @@
 package practice.regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
 public class TotalEarningsCalculatorRegex {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-  }
+    }
 
-  public static int calculateSalarySum(String text){
-    //TODO: реализуйте метод
-    return -1;
-  }
+    public static int calculateSalarySum(String text) {
+        //TODO: реализуйте метод
+        String[] substring = text.split(" ");
+        Pattern pattern = Pattern.compile("[0-9]");
+        int max = substring.length;
+        int earning = 0;
+        for (int numberOfString = 0; numberOfString < max; numberOfString++) {
+            Matcher mat = pattern.matcher(substring[numberOfString]);
+            if (mat.find() == true) {
+                earning = earning + Integer.parseInt(substring[numberOfString]);
+            }
+        }
+        return earning;
+    }
 
 }
