c61aed01ffdfdeb1aa3aa96bd145d107fba01560
AetemRadchuk <artem163ru199@mail.ru>
2022-03-27 12:20:35 +0400

    EmailList

---
+++ Array/EmailList/src/main/java/practice/EmailList.java
@@ -0,0 +1,31 @@
+package practice;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailList {
+    Set<String> emailList = new TreeSet<>();
+
+    public void add(String email) {
+        // TODO: валидный формат email добавляется, email это строка, она быть может любой
+        // принять решение добавлять аргумент email или нет должен этот метод
+        email = email.toLowerCase();
+        Pattern emailPattern = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = emailPattern.matcher(email);
+        boolean matches = matcher.matches();
+        if (matches == true) {
+            emailList.add(email);
+        } else {
+            System.out.println("Невалидный email");
+        }
+    }
+
+    public List<String> getSortedEmails() {
+        // TODO: возвращается сортированный список электронных адресов в алфавитном порядке
+        List<String> list = new ArrayList<>();
+        list.addAll(emailList);
+        return list;
+    }
+
+}
---
+++ Array/EmailList/src/main/java/practice/Main.java
@@ -0,0 +1,36 @@
+package practice;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static final String WRONG_EMAIL_ANSWER = "Неверный формат email";
+    
+    /* TODO:
+        Пример вывода списка Email, после ввода команды LIST в консоль:
+        test@test.com
+        hello@mail.ru
+        - каждый адрес с новой строки
+        - список должен быть отсортирован по алфавиту
+        - email в разных регистрах считается одинаковыми
+           hello@skillbox.ru == HeLLO@SKILLbox.RU
+        - вывод на печать должен быть в нижнем регистре
+           hello@skillbox.ru
+        Пример вывода сообщения об ошибке при неверном формате Email:
+        "Неверный формат email"
+    */
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
+            
+            //TODO: write code here
+            
+        }
+    }
+}
