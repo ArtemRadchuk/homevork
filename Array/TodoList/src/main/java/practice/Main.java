d20a91bcdf171bc55b3a825616d755236c75ddc6
AetemRadchuk <artem163ru199@mail.ru>
2022-04-06 10:11:05 +0400

    11

--- Array/TodoList/src/main/java/practice/Main.java
+++ Array/TodoList/src/main/java/practice/Main.java
@@ -1,11 +1,55 @@
 package practice;
 
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     private static TodoList todoList = new TodoList();
 
     public static void main(String[] args) {
-
         // TODO: написать консольное приложение для работы со списком дел todoList
+        String input = new Scanner(System.in).nextLine();
+        String command = input.substring(0, input.indexOf(" ")).toUpperCase(Locale.ROOT);
+        String inSide = input.substring(input.indexOf(" "), input.length()).trim();
+        Pattern pattern = Pattern.compile("[0-9]");
+        Matcher matcher = pattern.matcher(inSide);
+        while (input.equals("-1") == false) {
+            switch (command) {
+                case "ADD":
+                    if (matcher.find() == true) {
+                        String index = inSide.substring(0, inSide.indexOf(" "));
+                        int i = Integer.parseInt(index);
+                        String toDo = inSide.substring(i, inSide.length()-1);
+                        todoList.add(i, toDo);
 
+                    } else {
+                        todoList.add(inSide);
+                    }
+                    break;
+                case "LIST":
+                    todoList.getTodos();
+                    break;
+                case "EDIT":
+                    String indexEdit = inSide.substring(0, inSide.indexOf(" "));
+                    int iE = Integer.parseInt(indexEdit);
+                    String toDo = inSide.substring(inSide.indexOf(" "), inSide.length());
+                    todoList.edit(iE, toDo);
+                    break;
+                case "DELETE":
+                    String indexDelete = inSide.substring(0, inSide.length());
+                    int iD = Integer.parseInt(indexDelete);
+                    todoList.delete(iD);
+                    break;
+            }
+        }
+    }
+
+    enum command {
+        ADD,
+        LIST,
+        EDIT,
+        DELETE;
     }
 }
--- Array/TodoList/src/main/java/practice/TodoList.java
+++ Array/TodoList/src/main/java/practice/TodoList.java
@@ -1,8 +1,9 @@
 package practice;
+
 import java.util.ArrayList;
 
 public class TodoList {
-    ArrayList<String> ToDo = new ArrayList<>();
+    private ArrayList<String> ToDo = new ArrayList<>();
 
     public void add(String todo) {
         // TODO: добавьте переданное дело в конец списка
@@ -12,9 +13,9 @@ public class TodoList {
     public void add(int index, String todo) {
         // TODO: добавьте дело на указаный индекс,
         //  проверьте возможность добавления
-        if(index <= ToDo.size()){
-        ToDo.add(index,todo);}
-        else{
+        if (index <= ToDo.size()) {
+            ToDo.add(index, todo);
+        } else {
             ToDo.add(todo);
         }
     }
@@ -22,21 +23,22 @@ public class TodoList {
     public void edit(int index, String todo) {
         // TODO: заменить дело на index переданным todo индекс,
         //  проверьте возможность изменения
-        if(index <= ToDo.size()){
-        ToDo.set(index,todo);}
-
+        if (index <= ToDo.size()) {
+            ToDo.set(index, todo);
+        }
     }
 
     public void delete(int index) {
         // TODO: удалить дело находящееся по переданному индексу,
         //  проверьте возможность удаления дела
-        if(index < ToDo.size()){
-        ToDo.remove(index);}
+        if (index < ToDo.size()) {
+            ToDo.remove(index);
+        }
     }
 
     public ArrayList<String> getTodos() {
         // TODO: вернуть список дел
-
+        System.out.println(ToDo);
         return ToDo;
     }
 
