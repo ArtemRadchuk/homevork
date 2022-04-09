0f23b48db550ecdde746c0ca2762b6d40fcf242f
AetemRadchuk <artem163ru199@mail.ru>
2022-04-09 16:18:57 +0400

    Initializing

---
+++ ExceptionsDebuggingAndTesting/SPBMetro/src/test/java/RouteTest.java
@@ -0,0 +1,11 @@
+import core.Station;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RouteTest {
+    @Test
+    public void name() {
+     String stationFromName = "Девяткино";
+     String stationToName = "Лесная";
+    }
+}
