210f8d9acc4a76ac5fbcc392c43be861b4fdbf53
AetemRadchuk <artem163ru199@mail.ru>
2022-04-09 17:00:29 +0400

    fix2

--- Array/TodoList/src/main/java/practice/Main.java
+++ Array/TodoList/src/main/java/practice/Main.java
@@ -10,22 +10,33 @@ public class Main {
 
     public static void main(String[] args) {
         // TODO: написать консольное приложение для работы со списком дел todoList
-        String input = new Scanner(System.in).nextLine();
-        String command = input.substring(0, input.indexOf(" ")).toUpperCase(Locale.ROOT);
-        String inSide = input.substring(input.indexOf(" "), input.length()).trim();
-        Pattern pattern = Pattern.compile("[0-9]");
-        Matcher matcher = pattern.matcher(inSide);
-        while (input.equals("-1") == false) {
+        String command;
+        String inSide = "";
+        while (true) {
+            String input = new Scanner(System.in).nextLine();
+            String[] divineInput = input.split(" ");
+            if (divineInput.length > 1) {
+                command = input.substring(0, input.indexOf(" ")).toUpperCase(Locale.ROOT);
+                inSide = input.substring(input.indexOf(" "), input.length()).trim();
+            } else {
+                command = input.toUpperCase(Locale.ROOT);
+            }
+            Pattern pattern = Pattern.compile("[0-9]");
+            Matcher matcher = pattern.matcher(inSide);
+            if (input.equals("-1")) {
+                break;
+            }
             switch (command) {
                 case "ADD":
-                    if (matcher.find() == true) {
+                    if (matcher.find()) {
                         String index = inSide.substring(0, inSide.indexOf(" "));
                         int i = Integer.parseInt(index);
-                        String toDo = inSide.substring(i, inSide.length()-1);
+                        String toDo = inSide.substring(i, inSide.length() - 1);
                         todoList.add(i, toDo);
-
+                        System.out.println("Добавлено дело - " + toDo);
                     } else {
                         todoList.add(inSide);
+                        System.out.println("Добавлено дело - " + inSide);
                     }
                     break;
                 case "LIST":
@@ -42,11 +53,13 @@ public class Main {
                     int iD = Integer.parseInt(indexDelete);
                     todoList.delete(iD);
                     break;
+                default:
+                    System.out.println("Команда не распознана");
             }
         }
     }
 
-    enum command {
+    enum commandName {
         ADD,
         LIST,
         EDIT,
--- Array/TodoList/src/main/java/practice/TodoList.java
+++ Array/TodoList/src/main/java/practice/TodoList.java
@@ -3,43 +3,43 @@ package practice;
 import java.util.ArrayList;
 
 public class TodoList {
-    private ArrayList<String> ToDo = new ArrayList<>();
+    private ArrayList<String> toDo = new ArrayList<>();
 
     public void add(String todo) {
         // TODO: добавьте переданное дело в конец списка
-        ToDo.add(todo);
+        toDo.add(todo);
     }
 
     public void add(int index, String todo) {
         // TODO: добавьте дело на указаный индекс,
         //  проверьте возможность добавления
-        if (index <= ToDo.size()) {
-            ToDo.add(index, todo);
+        if (index <= toDo.size()) {
+            toDo.add(index, todo);
         } else {
-            ToDo.add(todo);
+            toDo.add(todo);
         }
     }
 
     public void edit(int index, String todo) {
         // TODO: заменить дело на index переданным todo индекс,
         //  проверьте возможность изменения
-        if (index <= ToDo.size()) {
-            ToDo.set(index, todo);
+        if (index <= toDo.size()) {
+            toDo.set(index, todo);
         }
     }
 
     public void delete(int index) {
         // TODO: удалить дело находящееся по переданному индексу,
         //  проверьте возможность удаления дела
-        if (index < ToDo.size()) {
-            ToDo.remove(index);
+        if (index < toDo.size()) {
+            toDo.remove(index);
         }
     }
 
     public ArrayList<String> getTodos() {
         // TODO: вернуть список дел
-        System.out.println(ToDo);
-        return ToDo;
+        System.out.println(toDo);
+        return toDo;
     }
 
 }
