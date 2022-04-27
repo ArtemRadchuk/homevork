19cd4714ae838604cacfe3c1874c2f05be4c1c95
AetemRadchuk <artem163ru199@mail.ru>
2022-04-27 13:09:03 +0400

    Теперь показывает название курса

--- DataBase/.idea/hw12.2.sql
+++ DataBase/.idea/hw12.2.sql
@@ -1,1 +1,1 @@
-select count(*) /  (select courses.duration from courses where id = course_id) from courses.subscriptions  group by course_id ;
+select (select name from courses where id = course_id), count(*) /  (select courses.duration from courses where id = course_id) from courses.subscriptions  group by course_id ;rse_id ;
