f130e43b0234dfbf493313778a2750960e5696fd
AetemRadchuk <artem163ru199@mail.ru>
2022-05-06 12:11:42 +0400

    Заполнил меин, перекинул файлы в ресурсы

--- FilesAndNetwork3/src/main/java/Main.java
+++ FilesAndNetwork3/src/main/java/Main.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        while (true) {
+            System.out.print("Введите путь к csv файлу");
+            Scanner scanner = new Scanner(System.in);
+            if (scanner.equals("-1")) {
+                break;
+            }
+            try {
+                Movements movements = new Movements(scanner.nextLine());
+                movements.getExpenseSum();
+                movements.getIncomeSum();
+            }catch (Exception e){
+                System.err.println("Ошибка в пути к файлу");
+            }
+
+        }
     }
 }
