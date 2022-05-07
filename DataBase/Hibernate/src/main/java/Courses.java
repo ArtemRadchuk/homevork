136e4b347cd9808a095c0836a5801d0a62acdde8
AetemRadchuk <artem163ru199@mail.ru>
2022-05-07 11:41:41 +0400

    Класс для таблици courses

---
+++ DataBase/Hibernate/src/main/java/Courses.java
@@ -0,0 +1,96 @@
+import javax.persistence.*;
+
+@Entity
+public class Courses {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "teacher_id")
+    private long teacher_id;
+
+    @Column(name = "student_count")
+    private int student_count;
+
+    @Column(name = "price")
+    private int price;
+
+    @Column(name = "price_per_hour")
+    private int price_per_hour;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getTeacher_id() {
+        return teacher_id;
+    }
+
+    public void setTeacher_id(long teacher_id) {
+        this.teacher_id = teacher_id;
+    }
+
+    public int getStudent_count() {
+        return student_count;
+    }
+
+    public void setStudent_count(int student_count) {
+        this.student_count = student_count;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getPrice_per_hour() {
+        return price_per_hour;
+    }
+
+    public void setPrice_per_hour(int price_per_hour) {
+        this.price_per_hour = price_per_hour;
+    }
+}
+
