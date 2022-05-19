71df6a30a1cc14bdef2616493a1e2a6f2e627048
ArtemRadchuk <100572270+ArtemRadchuk@users.noreply.github.com>
2022-05-19 13:15:00 +0400

    поправил имена

--- DataBase/Base.sql
+++ DataBase/Base.sql
@@ -9,62 +9,62 @@ CREATE TABLE IF NOT EXISTS public."Author"
     CONSTRAINT "Author_pkey" PRIMARY KEY ("Full_name")
 );
 
-CREATE TABLE IF NOT EXISTS public."Book"
+CREATE TABLE IF NOT EXISTS public."book"
 (
-    "Author" text COLLATE pg_catalog."default" NOT NULL,
-    "Name" text COLLATE pg_catalog."default" NOT NULL,
-    "Genre" text COLLATE pg_catalog."default" NOT NULL,
-    "Value" integer NOT NULL,
+    "author" text COLLATE pg_catalog."default" NOT NULL,
+    "name" text COLLATE pg_catalog."default" NOT NULL,
+    "genre" text COLLATE pg_catalog."default" NOT NULL,
+    "value" integer NOT NULL,
     book_id integer NOT NULL,
-    CONSTRAINT "Book_pkey" PRIMARY KEY (book_id)
+    CONSTRAINT "book_pkey" PRIMARY KEY (book_id)
 );
 
-CREATE TABLE IF NOT EXISTS public."Reader"
+CREATE TABLE IF NOT EXISTS public."reader"
 (
-    "Full_name" text COLLATE pg_catalog."default" NOT NULL,
-    "Basket" integer,
-    CONSTRAINT "Reader_pkey" PRIMARY KEY ("Full_name")
+    "full_name" text COLLATE pg_catalog."default" NOT NULL,
+    "basket" integer,
+    CONSTRAINT "reader_pkey" PRIMARY KEY ("full_name")
 );
 
 CREATE TABLE IF NOT EXISTS public.genre
 (
-    "Name" text COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT genre_pkey PRIMARY KEY ("Name")
+    "name" text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT genre_pkey PRIMARY KEY ("name")
 );
 
-ALTER TABLE IF EXISTS public."Book"
-    ADD CONSTRAINT author_fk FOREIGN KEY ("Author")
-    REFERENCES public."Author " ("Full_name") MATCH SIMPLE
+ALTER TABLE IF EXISTS public."book"
+    ADD CONSTRAINT author_fk FOREIGN KEY ("author")
+    REFERENCES public."author " ("full_name") MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
 
 
-ALTER TABLE IF EXISTS public."Book"
-    ADD CONSTRAINT genre_fk FOREIGN KEY ("Genre")
-    REFERENCES public.genre ("Name") MATCH SIMPLE
+ALTER TABLE IF EXISTS public."book"
+    ADD CONSTRAINT genre_fk FOREIGN KEY ("genre")
+    REFERENCES public.genre ("name") MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
 
 
-ALTER TABLE IF EXISTS public."Reader"
-    ADD CONSTRAINT "Basket_fk" FOREIGN KEY ("Basket")
-    REFERENCES public."Book" (book_id) MATCH SIMPLE
+ALTER TABLE IF EXISTS public."reader"
+    ADD CONSTRAINT "basket_fk" FOREIGN KEY ("basket")
+    REFERENCES public."book" (book_id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
-CREATE INDEX IF NOT EXISTS "Basket"
-    ON public."Reader"("Basket");
+CREATE INDEX IF NOT EXISTS "basket"
+    ON public."reader"("basket");
 
 
-ALTER TABLE IF EXISTS public."Reader"
-    ADD CONSTRAINT "Basket_fk2" FOREIGN KEY ("Basket")
-    REFERENCES public."Book" ("Value") MATCH SIMPLE
+ALTER TABLE IF EXISTS public."reader"
+    ADD CONSTRAINT "basket_fk2" FOREIGN KEY ("basket")
+    REFERENCES public."book" ("value") MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
-CREATE INDEX IF NOT EXISTS "Basket"
-    ON public."Reader"("Basket");
+CREATE INDEX IF NOT EXISTS "basket"
+    ON public."reader"("basket");
 
 END;
