731bde91da139f398a53a8d572399f679bdf992e
ArtemRadchuk <100572270+ArtemRadchuk@users.noreply.github.com>
2022-05-25 15:16:05 +0400

    desktop0,2

--- desktop-app-16/src/main/java/com/example/app/desktopapp16/AppApplication.java
+++ desktop-app-16/src/main/java/com/example/app/desktopapp16/AppApplication.java
@@ -10,13 +10,17 @@ import java.io.IOException;
 
 public class AppApplication extends Application {
     public static Stage stage;
+    public static Parent root;
+
+    public AppApplication() throws IOException {
+    }
 
     @Override
     public void start(Stage stage) throws IOException {
         stage.setTitle("Введите ФИО");
         stage.setHeight(360);
         stage.setWidth(300);
-        Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
+        root = FXMLLoader.load(getClass().getResource("view.fxml"));
         stage.setScene(new Scene(root));
         this.stage = stage;
         stage.show();
--- desktop-app-16/src/main/java/com/example/app/desktopapp16/controller/AppController.java
+++ desktop-app-16/src/main/java/com/example/app/desktopapp16/controller/AppController.java
@@ -18,11 +18,12 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 public class AppController {
     private Stage stage;
     private Scene scene;
-
+    /*private Parent root;*/
     @FXML
     private TextField surName;
     @FXML
@@ -32,7 +33,7 @@ public class AppController {
 
     @FXML
     protected void onButtonClick() throws Exception {
-        if (new ValidateServiceImpl().validate(surName, name , patronymic)) {
+        if (new ValidateServiceImpl().validate(surName, name, patronymic)) {
             switchScene();
         } else {
             Stage stage = new Stage();
@@ -61,40 +62,38 @@ public class AppController {
     public void switchScene() throws IOException {
         HBox hBox = new HBox();
         VBox vBox = new VBox();
+
         Button button = new Button("Expand");
         button.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 stage.setTitle("Введите ФИО");
-                stage.setHeight(360);
-                stage.setWidth(300);
-                Parent root = null;
                 try {
-                    root = FXMLLoader.load(getClass().getResource("view.fxml"));
+                    Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
+                    stage.setScene(new Scene(root));
+                    stage.show();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                stage.setScene(new Scene(root));
-                stage.show();
             }
         });
         Label surNameLabel = new Label(surName.getText());
         Label nameLabel = new Label(name.getText());
         hBox.getChildren().addAll(surNameLabel, nameLabel);
         if (!patronymic.getText().isEmpty()) {
-        Label patronymicLabel = new Label(patronymic.getText());
-        hBox.getChildren().add(patronymicLabel);
+            Label patronymicLabel = new Label(patronymic.getText());
+            hBox.getChildren().add(patronymicLabel);
         }
         hBox.setSpacing(10);
-        hBox.setAlignment(Pos.TOP_CENTER);
-        vBox.getChildren().addAll(hBox,button);
+        vBox.getChildren().addAll(hBox, button);
         stage = AppApplication.stage;
+        stage.setWidth(300);
+        stage.setHeight(360);
         hBox.setAlignment(Pos.CENTER);
         vBox.setAlignment(Pos.CENTER);
         scene = new Scene(vBox);
         stage.setScene(scene);
-        stage.setWidth(300);
-        stage.setHeight(360);
+
         stage.show();
     }
 }
