6f2f344035adad16c3fbdcb8189e1758131dc39c
ArtemRadchuk <100572270+ArtemRadchuk@users.noreply.github.com>
2022-06-09 17:32:58 +0400

    /

--- crud-app/src/main/java/controller/GenreController.java
+++ crud-app/src/main/java/controller/GenreController.java
@@ -11,17 +11,17 @@ import service.impl.GenreServiceImpl;
 public class GenreController {
     private GenreServiceImpl genreService;
 
-    @PostMapping(value = "/author/add")
+    @PostMapping(value = "/genre/add")
     public void add(@RequestBody String name) {
         genreService.createGenre(name);
     }
 
-    @GetMapping(value = "/authors")
+    @GetMapping(value = "/genres")
     public void getGenre(Model model) {
         model.addAttribute("", genreService.genreList());
     }
 
-    @GetMapping(value = "/author/delete")
+    @GetMapping(value = "/genre/delete")
     public void deleteGenre(int id) {
         genreService.deleteGenre(id);
     }
